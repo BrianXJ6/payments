fec5ce2d6bac00093143027173338f5c42c72b57
Brian Ferreira <brianferreira15@hotmail.com>
2021-06-25 14:36:10 -0300

    Ajustes na rota web para telas iniciais gnet; Criadas rotas da api para pagamentos gnet

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\Gnet\CardController;
+use App\Http\Controllers\API\Gnet\CallbackController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Gnet group
+Route::prefix('payment/gnet')->group(function () {
+    Route::post('card', CardController::class);
+    Route::post('status', CallbackController::class);
 });

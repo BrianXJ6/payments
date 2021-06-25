f2d7f6cf9c72399f60de8e48860618b0a715eb7c
Brian Ferreira <brianferreira15@hotmail.com>
2021-06-25 18:24:02 -0300

    Finalizada rota para callback e update status do pedido

--- app/Http/Controllers/API/Gnet/CallbackController.php
+++ app/Http/Controllers/API/Gnet/CallbackController.php
@@ -2,12 +2,41 @@
 
 namespace App\Http\Controllers\API\Gnet;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
+use Gerencianet\Gerencianet;
 use App\Http\Controllers\Controller;
 
 class CallbackController extends Controller {
 
     public function __invoke(Request $request) {
-        //
+        $request->validate(['notification' => ['required', 'string']]);
+        try {
+            // Iniciando API Gerencianet
+            $api = new Gerencianet([
+                'client_id'     => config('gateway.gnet_client_id'),
+                'client_secret' => config('gateway.gnet_client_secret'),
+                'sandbox'       => true,
+            ]);
+
+            // Executando consulta de status (notification)
+            $chargeNotification = $api->getNotification(['token' => $request->notification], []);
+            if ($chargeNotification['code'] !== 200) return response()->json(['message' => 'Não foi possível atender essa solicitação'], 422);
+
+            // Obtendo status e identificação do pedido
+            $lastNotification = $chargeNotification['data'][count($chargeNotification['data']) - 1];
+            $paid_at          = $lastNotification['created_at'];
+            $newStatus        = $lastNotification['status']['current'];
+            $payment_id       = $lastNotification['identifiers']['charge_id'];
+
+            // Atualizando o status do pedido
+            $order          = Order::where('payment_id', $payment_id)->firstOrFail();
+            $order->status  = $newStatus;
+            $order->paid_at = $paid_at;
+            $order->save();
+            return response()->json([], 204);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
     }
 }

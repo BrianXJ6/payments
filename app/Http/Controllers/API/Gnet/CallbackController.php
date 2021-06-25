d892d59693bd19bc7ef7b93aac6c0b448aae2b8e
Brian Ferreira <brianferreira15@hotmail.com>
2021-06-25 14:40:00 -0300

    Criado controlador para Callbacks do servidor gnet.

---
+++ app/Http/Controllers/API/Gnet/CallbackController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\API\Gnet;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CallbackController extends Controller {
+
+    public function __invoke(Request $request) {
+        //
+    }
+}
